7bc565ca6548428e956818617057309fbf5f7469
glstrachan <70994520+glstrachan@users.noreply.github.com>
2021-06-07 13:50:47 -0500

    Project Files
    The necessary files for data management and storage

---
+++ project/app/src/main/java/com/healthapp/LogData.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class LogData implements java.io.Serializable {
+	private String firstName;
+	private String lastName;
+	private ArrayList<DataInstance> data = new ArrayList<DataInstance>();
+
+	public LogData(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public ArrayList<DataInstance> getData() {
+		return data;
+	}
+
+	public DataInstance getDataInstance(int i) {
+		return data.get(i);
+	}
+
+	public DataInstance getDataInstance() {
+		return data.get(data.size() - 1);
+	}
+
+	public void addDataInstance(DataInstance newInstance) {
+		data.add(newInstance);
+	}
+
+	public void removeDataInstance(int i) {
+		data.remove(i);
+	}
+}
